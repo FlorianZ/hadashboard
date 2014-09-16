ddafbf5dc180215a16f3974a6e28f11b603896e5
Florian Zitzelsberger <florian.zitzelsberger@gmail.com>
2014-09-15 20:42:47 -0700

    Adding comments to settings code.

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,7 +1,10 @@
 require 'data_mapper'
 
+# Initialize the DataMapper to use a database, if available. Fall back to an
+# sqlite file, if no database has been set up.
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite:persistent.db')
 
+# Set object model for settings
 class Setting
   include DataMapper::Resource
 
@@ -9,5 +12,8 @@ class Setting
   property :value, Text
 end
 
+# Finalize all models
 DataMapper.finalize
+
+# Up-migrate the schema
 DataMapper.auto_upgrade!
