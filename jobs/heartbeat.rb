bb9a992fec39d7d1a96a413e656acc26006ba3ea
Florian Zitzelsberger <florian.zitzelsberger@gmail.com>
2014-09-17 21:45:54 -0700

    Experimenting with a higher heartbeat delay.

--- jobs/heartbeat.rb
+++ jobs/heartbeat.rb
@@ -1,5 +1,5 @@
 # Send a heartbeat message to keep the event stream open
-SCHEDULER.every '7s', :first_in => 0 do |job|
-  event = "heartbeatTimeout: 15000\n\n"
+SCHEDULER.every '30s', :first_in => 0 do |job|
+  event = "heartbeatTimeout: 45000\n\n"
   Sinatra::Application.settings.connections.each { |out| out << event }
 end
