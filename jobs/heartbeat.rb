825daf966e1fe182f37352a27872a6c77c33f9f4
Florian Zitzelsberger <florian.zitzelsberger@gmail.com>
2014-09-17 22:46:53 -0700

    Experimenting with starving EventSource of a heartbeat.

--- jobs/heartbeat.rb
+++ jobs/heartbeat.rb
@@ -1,5 +1,5 @@
 # Send a heartbeat message to keep the event stream open
-SCHEDULER.every '30s', :first_in => 0 do |job|
+SCHEDULER.every '10m', :first_in => 0 do |job|
   event = "heartbeatTimeout: 45000\n\n"
   Sinatra::Application.settings.connections.each { |out| out << event }
 end
