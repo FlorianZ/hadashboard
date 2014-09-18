e12e6398c03f4a768d6ff6f4e31d79cdd77b5658
Florian Zitzelsberger <florian.zitzelsberger@gmail.com>
2014-09-18 08:30:05 -0700

    Using a commet message as the heartbeat.

--- jobs/heartbeat.rb
+++ jobs/heartbeat.rb
@@ -1,5 +1,5 @@
 # Send a heartbeat message to keep the event stream open
-SCHEDULER.every '10m', :first_in => 0 do |job|
-  event = "heartbeatTimeout: 45000\n\n"
+SCHEDULER.every '15s', :first_in => 0 do |job|
+  event = ":heartbeat #{Time.now.to_i}\n\n"
   Sinatra::Application.settings.connections.each { |out| out << event }
 end
